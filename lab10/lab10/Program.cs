a939901894610f8d0c78d17e7894e2745de2efb9
Nerfay <101548076+Nerfay@users.noreply.github.com>
2023-11-29 22:39:26 +0200

    Add files via upload

---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace lab10;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Person[] persons = new Person[1000000];
+
+        TaskManager taskManager = new TaskManager();
+        var taskInfo = taskManager.Generate(persons);
+
+        await taskInfo;
+        
+        // Sort in one thread
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var comparer =
+            new PersonComparer("Name");
+        Array.Sort(persons, comparer);
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        
+        stopwatch.Reset();
+        stopwatch.Start();
+        // Sort in some threads
+        taskInfo = taskManager.ParallelSort(persons, 4);
+
+        await taskInfo;
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+}
